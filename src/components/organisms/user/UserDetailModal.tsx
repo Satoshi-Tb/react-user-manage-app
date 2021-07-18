f59f3ef4020627884043ca80baa7a159d5c4d016
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2021-07-18 09:30:26 +0900

    管理者のみ編集導線追加

--- src/components/organisms/user/UserDetailModal.tsx
+++ src/components/organisms/user/UserDetailModal.tsx
@@ -1,4 +1,4 @@
-import { memo, VFC } from "react";
+import { memo, useState, VFC, useEffect, ChangeEvent } from "react";
 import {
   Modal,
   ModalContent,
@@ -18,13 +18,41 @@ import { User } from "../../../types/api/user";
 type Props = {
   user: User | null;
   isOpen: boolean;
+  isAdmin?: boolean;
   onClose: () => void;
 };
 
 export const UserDetailModal: VFC<Props> = memo((props) => {
-  const { isOpen, onClose, user } = props;
+  console.log("UserDetailModal start");
+  const { isOpen, onClose, isAdmin = false, user } = props;
+  console.log(user);
+  const [username, setUsername] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  useEffect(() => {
+    setUsername(user?.username ?? "");
+    setName(user?.name ?? "");
+    setEmail(user?.email ?? "");
+    setPhone(user?.phone ?? "");
+  }, [user]);
+
+  const onChangeName = (event: ChangeEvent<HTMLInputElement>) =>
+    setName(event.target.value);
+
+  const onChangeUsername = (event: ChangeEvent<HTMLInputElement>) =>
+    setUsername(event.target.value);
+
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>) =>
+    setEmail(event.target.value);
+
+  const onChangePhone = (event: ChangeEvent<HTMLInputElement>) =>
+    setPhone(event.target.value);
+
   const onClickUpdate = () => {
     alert("更新");
+    //console.log(`name=${name}`);
   };
   return (
     <Modal
@@ -41,21 +69,37 @@ export const UserDetailModal: VFC<Props> = memo((props) => {
           <Stack spacing={4}>
             <FormControl>
               <FormLabel>名前</FormLabel>
-              <Input value={user?.name} isReadOnly />
+              <Input
+                value={name}
+                isReadOnly={!isAdmin}
+                onChange={onChangeName}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>フルネーム</FormLabel>
-              <Input value={user?.username} isReadOnly />
+              <Input
+                value={username}
+                isReadOnly={!isAdmin}
+                onChange={onChangeUsername}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>メール</FormLabel>
-              <Input value={user?.email} isReadOnly />
+              <Input
+                value={email}
+                isReadOnly={!isAdmin}
+                onChange={onChangeEmail}
+              />
             </FormControl>
             <FormControl>
               <FormLabel>TEL</FormLabel>
-              <Input value={user?.phone} isReadOnly />
+              <Input
+                value={phone}
+                isReadOnly={!isAdmin}
+                onChange={onChangePhone}
+              />
             </FormControl>
-            {false ? <Button onClick={onClickUpdate}>更新</Button> : <></>}
+            {isAdmin && <Button onClick={onClickUpdate}>更新</Button>}
           </Stack>
         </ModalBody>
       </ModalContent>
